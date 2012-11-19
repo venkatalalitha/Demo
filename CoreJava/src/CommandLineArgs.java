83ec18bc0be454886a6c8c2a3a78b6d85fb278a8
Venkatalalitha.Rao <Venkatalalitha.Rao@14425-417b3d182>
2012-11-19 15:44:30 +0530

    hi

---
+++ CoreJava/src/CommandLineArgs.java
@@ -0,0 +1,15 @@
+
+public class CommandLineArgs {
+
+	public static void main(String[] args) {
+		  
+		System.out.println("The length of the arguments=="+args.length);
+		System.out.println(args[0]);
+		
+		/*for(int i=0;i<=args.length;i++){
+			System.out.println(args[i]);
+		}*/
+			
+		}
+	}
+
---
+++ CoreJava/src/VariableD.java
@@ -0,0 +1,16 @@
+
+public class VariableD {
+	
+	public static void main(String[] args) {
+		int num=0;
+		int num1=4,num2=5;
+		int sum=num1+num2;  
+		for(num=0;num<=10;num++){
+			System.out.println(num);
+	        
+		}
+		System.out.println("sum of the two numbers==>"+sum);
+		
+	}
+
+}
